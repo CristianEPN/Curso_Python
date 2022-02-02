376e23e27b6b735a061fb845751260c531f3efad
Cristian López <cristian.lopez02@epn.edu.ec>
2022-02-01 20:03:48 -0500

    Primero pasos en PYTHON

---
+++ FUNDAMENTOS/1.Input_Output.py
@@ -0,0 +1,21 @@
+#Funcion entrada y funcion salida
+#Buscar palabras reservadas 
+
+#Pasar argumento a la función, "" -> string
+print("Formulario")
+print("Ingrese su nombre: ", end="")
+nombre = input()
+print("Ingrese su apellido: ", end="")
+apellido = input()
+print("Ingrese su edad: ", end="")
+edad = input()
+
+#Primer reto del curso
+ciudad = input ("Ingresa tu ciudad: ")
+
+print("Su nombre es: ",nombre)
+print("Su apellido es: ",apellido)
+print("Su edad es: ",edad," años")
+print("Es de la ciudad de: ",ciudad)
+
+#PYTHON es un lenguaje 100% orientado a objetos
---
+++ FUNDAMENTOS/2.Variables.py
@@ -0,0 +1,20 @@
+#Variables
+#NO debe tener caracteres especial, NO debe empezar en mayusculas, no debe ser palabra reservada
+
+#Tipos de Variables en PYTHON
+#PYTHON es un lenguaje de tipado debil
+
+#Inicialización de Variables -> 4. El tipo de dato de las variables viene dado por la inicialización
+#cast -> casting de variables
+edad = 22
+nombre = "Cristian"
+#Declaracion de los flotantes es con PUNTO (.)
+ira = 37.2
+mayorEdad = True
+
+print("El tipo de dato de la variable edad es: ", type(edad))
+print("El tipo de dato de la variable nombre es: ", type(nombre))
+print("El tipo de dato de la variable ira es: ", type(ira))
+print("El tipo de dato de la variable mayorEdad es: ", type(mayorEdad))
+
+
